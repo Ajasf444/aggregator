4014d54f878f63683e8c9176c629b8654f6e3cff
Ajasf444 <longstephen444@gmail.com>
2026-03-17 00:24:56 -0500

    added functionality to Read()

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,30 @@
 package config
 
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	db_url            string
-	current_user_name string
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read(location string) Config {
-	return Config{}
+func Read(location string) (Config, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, errors.New("unable to retrieve home directory")
+	}
+	content, err := os.ReadFile(home + configFileName)
+	if err != nil {
+		return Config{}, errors.New("unable to read config file")
+	}
+	config := Config{}
+	if err := json.Unmarshal(content, &config); err != nil {
+		return Config{}, errors.New("unable to unmarshal config file")
+	}
+	return config, nil
 }
