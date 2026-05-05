51a72ea011d3eacfda226570bf927079feaa1055
Ajasf444 <longstephen444@gmail.com>
2026-05-05 00:05:20 -0500

    created handlerGetUsers() and registered it with main(), also reordered some logic in handlerRegister()

--- commands.go
+++ commands.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/Ajasf444/aggregator/internal/config"
@@ -62,8 +63,8 @@ func handlerRegister(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return errors.New("register command expecting name argument")
 	}
-	params := database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: cmd.args[0]}
 	ctx := context.Background()
+	params := database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: cmd.args[0]}
 	user, err := s.db.CreateUser(ctx, params)
 	if err != nil {
 		return err
@@ -81,5 +82,25 @@ func handlerReset(s *state, cmd command) error {
 	ctx := context.Background()
 	s.db.DeleteUsers(ctx)
 	fmt.Println("Database reset.")
+	return nil
+}
+
+func handlerGetUsers(s *state, cmd command) error {
+	ctx := context.Background()
+	users, err := s.db.GetUsers(ctx)
+	if err != nil {
+		return err
+	}
+	// TODO: optimization would be to build the string here rather than invoke strings.Join()
+	found := false
+	for i, user := range users {
+		if !found && (user == s.cfg.CurrentUserName) {
+			found = true
+			users[i] += " (current)"
+			user = users[i]
+		}
+		users[i] = "* " + user
+	}
+	fmt.Println(strings.Join(users, "\n"))
 	return nil
 }
