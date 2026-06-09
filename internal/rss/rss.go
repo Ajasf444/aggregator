307f99233691159a14900ed28b53a9484b610e1d
Ajasf444 <longstephen444@gmail.com>
2026-06-09 00:04:58 -0500

    added a TODO

--- internal/rss/rss.go
+++ internal/rss/rss.go
@@ -43,6 +43,7 @@ func FetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 		return &RSSFeed{}, err
 	}
 	req.Header.Set("User-Agent", "gator")
+	// TODO: Create a Client with a Timeout
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return &RSSFeed{}, err
